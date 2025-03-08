59c670cf7c1ebf7d6102c3e6fcb6138cd236121d
MarkRyuo <malupajhonmark3035@gmail.com>
2025-03-08 21:56:26 +0800

    Subject: Update

--- script.js
+++ script.js
@@ -21,9 +21,9 @@ let result = Math.round(num1 * num2) ;
 let num3 = 1.2 ;
 let num4 = Math.pow(2, 5) ;
 
-const result2 = num3 * num
+const result2 = num3 * num4 ;
 
 
 // let result = num1 * num2;
 
-console.log(result)
+console.log(result, result2)
